b9f42a164210ee72d8c41674aacb9481ed3defe4
Aaska Koirala <aaskakoirala@gmail.com>
2026-01-15 08:48:54 +0545

    added code for week2 day1

---
+++ Week2/Day1/d1q2.cpp
@@ -0,0 +1,17 @@
+// Q2 Write a C++ program to find the greater of two numbers.
+
+#include <iostream>
+using namespace std;
+int main() {
+    double num1, num2;
+    cout << "Dui ota number halnu hos: ";
+    cin >> num1 >> num2;
+    if (num1 > num2) {
+        cout << "Duita madhye thulo number is: " << num1 << endl;
+    } else if (num2 > num1) {
+        cout << "Duita madhye thulo number is: " << num2 << endl;
+    } else {
+        cout << "Dui ota number equal chan." << endl;
+    }
+    return 0;
+}
---
+++ Week2/Day1/srand.cpp
@@ -0,0 +1,13 @@
+//IMP: random number generating (using srand give random numbers, without it gives same number everytinme)
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main() {
+
+       srand(time(0));
+int number = rand() % 10;
+
+cout << "Generated random number is: " << number << endl;
+return 0;
+
+}
