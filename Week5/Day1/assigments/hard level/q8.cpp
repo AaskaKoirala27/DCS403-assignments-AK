bd478c134082811d5785804932468e1d397d13c7
Aaska Koirala <aaskakoirala@gmail.com>
2026-02-05 22:49:06 +0545

    added assignments for week5 day1

---
+++ Week5/Day1/assigments/hard level/q8.cpp
@@ -0,0 +1,24 @@
+// Q8. Reverse a String Using Recursion
+// Reverse a given string using recursion.
+
+#include <iostream>
+using namespace std;
+
+void reverseString(string &s, int index) {
+    if (index < 0) {          
+        return;
+    }
+    cout << s[index];        
+    reverseString(s, index - 1); 
+}
+
+int main() {
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
+
+    cout << "Reversed string is: ";
+    reverseString(s, s.length() - 1); 
+    cout << endl;
+    return 0;
+}
